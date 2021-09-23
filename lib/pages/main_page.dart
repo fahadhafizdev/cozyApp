9839f0c0bdd72a324a26505625884b7b41a0b5f8
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-23 18:44:54 +0700

    Create main_page.dart

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pencarian_kos/theme.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
