4463380887d3c14a192a08e82252e1fd0a09979e
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-28 17:51:38 +0700

    Update page_provider.dart

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,1 +1,12 @@
-class PageProvider {}
+import 'package:flutter/cupertino.dart';
+
+class PageProvider with ChangeNotifier {
+  int _number = 0;
+
+  int get number => _number;
+
+  set number(int newValue) {
+    _number = newValue;
+    notifyListeners();
+  }
+}
